a85deab79a0dced1909571c046917ab85eb4006b
Andreas Gabriel Vannas <jaanus.maddison@gmail.com>
2024-07-21 23:39:07 +0300

    add proper error handleling to sql create functions

--- app/dbAct/create.js
+++ app/dbAct/create.js
@@ -13,12 +13,21 @@ function generateCreateUserQuery(dup){
 }
 
 // Creates a user with required parameters to users table
-function createUser(steam64ID, discordName, age, email, rank, dupCheck = true){
+async function createUser(steam64ID, discordName, age, email, rank, dupCheck = true){
     const query = generateCreateUserQuery(dupCheck);
-    
-    sql.query(query, [steam64ID, discordName, age, email, rank], err=>{
-        handleError(err);
-        outLog(`User created with steamid ${steam64ID}. Users discord: ${discordName}.`)
+
+    await sql.promise().query(query, [steam64ID, discordName, age, email, rank])
+        .then(result => {
+            output = (`User created with steamid ${steam64ID}. Users discord: ${discordName}.`)
+            outLog(output);
+        })
+        .catch(error => {
+            outLog(`User could not be created.`)
+            console.error(error)
+            output = `User couldn't be created.`
+        });
+    return new Promise((resolve)=>{
+        resolve(output);
     });
 }
 // Create User is used in ./main.js
@@ -26,7 +35,7 @@ exports.createUser = createUser;
 
 
 // Function to create new items into the database
-function createTable(name, fieldsIn) {
+async function createTable(name, fieldsIn) {
     // change array into sql formated fields
     const fields = fieldsIn.map(field => {
         return `${sql.escapeId(field.key)} ${field.value}`;
@@ -34,9 +43,18 @@ function createTable(name, fieldsIn) {
     // Create query
     const query = `CREATE TABLE IF NOT EXISTS ${sql.escapeId(name)} (${fields});`;
     // create the table
-    sql.query(query, (err) => {
-        handleError(err);
-        outLog(`Created table ${name}`);
+    await sql.promise().query(query)
+        .then(result => {
+            output = (`Table ${name} was created.`)
+            outLog(output);
+        })
+        .catch(error => {
+            output = `Table couldn't be created.` 
+            outLog(output)
+            console.error(error)
+        });
+    return new Promise((resolve)=>{
+        resolve(output);
     });
 }
 exports.createTable = createTable;
